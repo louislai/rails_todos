085e9b8e4dcbc819eda41ac148eaabb43a2cb597
Louis Lai <hoangdung2506@gmail.com>
2015-01-17 13:05:24 +0800

    Change

--- db/migrate/20150109070505_create_tasks.rb
+++ db/migrate/20150109070505_create_tasks.rb
@@ -3,7 +3,7 @@ class CreateTasks < ActiveRecord::Migration
     create_table :tasks do |t|
       t.string :title
       t.text :body
-      t.date :deadline
+      t.datetime :deadline
       t.belongs_to :user, index: true
       t.timestamps null: false
     end
