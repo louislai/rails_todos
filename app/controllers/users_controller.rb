dbdec9b509d21f355053005d3761af3d27f01197
Louis Lai <hoangdung2506@gmail.com>
2015-01-20 11:59:48 +0800

    Fix signup error

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -30,16 +30,13 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
     respond_to do |format|
       if @user.save
-        @user.tasklists.create(title: "First Tasklist", description: "This is your first tasklist")
         log_in @user
         format.html do 
           flash[:success] = "Your account has been created successfully!"
           redirect_to @user
         end
-        format.json { render :show, status: :created, location: @user }
       else
         format.html { render :new }
-        format.json { render json: @user.errors, status: :unprocessable_entity }
       end
     end
   end
